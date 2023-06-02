d4f9b7d8ba80c92976a34c5bc52c4147ac69d3ec
ranv1994 <ranveer@entitysport.com>
2023-06-02 12:18:46 +0530

    First push

--- pre-work.go
+++ pre-work.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"database/sql"
+	// "database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,78 +9,51 @@ import (
 	"net/http"
 	"strings"
 	"sync"
-	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	// _ "github.com/go-sql-driver/mysql"
 )
 
 // Define an array to store the client endpoint URLs
-var clientUrls []string
-
-// Define a struct for the log data
-type LogData struct {
-	EndpointURL string    `json:"endpoint_url"`
-	Status      string    `json:"status"`
-	Timestamp   time.Time `json:"timestamp"`
-}
-
-// Function to fetch client endpoint URLs from the database
-func fetchClientUrlsFromDatabase() error {
-	// Establish a database connection
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3310)/user_dash")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	// Execute the query to fetch the endpoint URLs
-	rows, err := db.Query("SELECT test_url_column as url FROM es_user_subscriptions")
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-
-	// Iterate over the query results and populate the clientUrls array
-	for rows.Next() {
-		var url string
-		err = rows.Scan(&url)
-		if err != nil {
-			return err
-		}
-		clientUrls = append(clientUrls, url)
-	}
-
-	err = rows.Err()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Function to save the log data to the database
-func saveLogToDatabase(data LogData) error {
-	// Establish a database connection
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3310)/user_dash")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	// Prepare the SQL statement
-	stmt, err := db.Prepare("INSERT INTO test_logs(endpoint_url, status, timestamp) VALUES(?, ?, ?)")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	// Execute the SQL statement with the log data
-	_, err = stmt.Exec(data.EndpointURL, data.Status, data.Timestamp)
-	if err != nil {
-		return err
-	}
-
-	return nil
+var clientUrls = []string{
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
+	"http://tennis.entitysport.com/welcome/mt_test_webhook",
+	"https://tennis.entitysport.com/cron/testpush.php",
+	"https://polls.iplt20.com/widget/cron/testpush.php",
 }
 
 // Function to send JSON data to multiple endpoints concurrently
@@ -100,18 +73,7 @@ func sendJsonDataToMultipleEndpoints(data map[string]interface{}, wg *sync.WaitG
 			continue
 		}
 		defer resp.Body.Close()
-		// Prepare the log data
-		logData := LogData{
-			EndpointURL: url,
-			Status:      "success", // or "failure" based on the response status
-			Timestamp:   time.Now(),
-		}
 
-		// Save the log data to the database
-		err3 := saveLogToDatabase(logData)
-		if err3 != nil {
-			log.Printf("Error saving log to the database: %v\n", err3)
-		}
 		log.Printf("Data sent to %s. Response: %d\n", url, resp.StatusCode)
 	}
 }
@@ -136,12 +98,6 @@ func triggerDataSending(w http.ResponseWriter, r *http.Request) {
 	err = json.Unmarshal(jsonData, &data)
 	if err != nil {
 		log.Fatalf("Error unmarshaling JSON data: %v\n", err)
-	}
-
-	// Fetch client endpoint URLs from the database
-	err2 := fetchClientUrlsFromDatabase()
-	if err2 != nil {
-		log.Fatalf("Error fetching client URLs: %v\n", err2)
 	}
 
 	var wg sync.WaitGroup
